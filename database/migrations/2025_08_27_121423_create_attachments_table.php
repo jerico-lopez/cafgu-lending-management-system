5e84aeb714d7badd132000f19a8c30bb7f667891
izme170 <jericolopez2003@gmail.com>
2025-08-27 23:35:15 +0800

    add file_name column

--- database/migrations/2025_08_27_121423_create_attachments_table.php
+++ database/migrations/2025_08_27_121423_create_attachments_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('attachments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('member_id')->constrained()->onDelete('cascade');
-            $table->string('file_path');
+            $table->string('file_name', 50);
+            $table->string('file_path', 255);
             $table->timestamps();
         });
     }
